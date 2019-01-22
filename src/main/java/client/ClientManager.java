96711b2336f31c41329b3cf8887f4f3e0024e8a4
Siraj Ahmadzai <sirajahmadzai@cmail.carleton.ca>
2019-01-22 09:24:06 -0800

    Screen Adaptatives and Sequential reverting

--- src/main/java/client/ClientManager.java
+++ src/main/java/client/ClientManager.java
@@ -259,6 +259,7 @@ public class ClientManager implements clientManagerInterface {
         if (!isOwnerTurn()) {
             return;
         }
+        gameView.takeSnapshot();
 
         if (!selectionManager.isEmpty()) {
             // Move all selected cards to the new set.
@@ -315,6 +316,9 @@ public class ClientManager implements clientManagerInterface {
 
 //      No card played. Just pass the turn.
         if (prevHand.equals(lastHand)) {
+            /*My Code */
+                gameView.init_snapstate();
+            /*End */
             client.sendCommandToServer(new PassTurn());
             return true;
         }
@@ -372,6 +376,10 @@ public class ClientManager implements clientManagerInterface {
 //        List<CardSet> cardsOnTheTable = gameView.getPlayArea().getSnapshot();
         gameView.getPlayArea().setAllSets(move.getTable());
         gameView.removeCardsFrom(move.getSeatNumber(), move.getPlayedCards());
+        /*  My Code 2019-01-20-12-22*   At turn starting recorde the table */
+        gameView.init_snapstate();
+        gameView.takeSnapshot();
+        /*                */
         startTurn();
 
 //        Remove cards from hand!
